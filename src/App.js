66d92d6a8372ec05454ca5dbd999fca74d036590
Archana00N <b20082@students.iitmandi.ac.in>
2022-01-09 17:36:13 +0530

    techsoc page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from 'react'
-import Navbar from './components/Navbar'
- const App = () => {
-  return <Navbar />
-}
+import React from "react";
+// import Navbar from "./components/Navbar";
+import Techclubs from "./components/pages/Techclubs";
+const App = () => {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      <Techclubs />
+    </div>
+  );
+};
 export default App;
